3864178d223c09f801df4da4989bad16a47de3f8
Kirihito <satowa.network@gmail.com>
2021-12-15 19:27:16 +0100

    Added other.php
    other php ist eine php Datei wo alle variablen und all der Code steht der direct ausgeführt werden muss BEVOR der html tag beginnt somit eine gute Chance für wichtige Scripts. Derzeit besteht der aus dem ganz leichten und simplen Title import

---
+++ assets/php/other.php
@@ -0,0 +1,10 @@
+<?php
+////////////////////////////////////////////////////
+//  Copyright by: Satowa Network                  //
+//  Author: Kirihito                              //
+//  Discord: https://satowa-network.at/discord    //
+//  Email: support@satowa-network.at              //
+//  Website: https://satowa-network.at            //
+////////////////////////////////////////////////////
+$titlea="Satowa Network | ";
+$title = $titlea.$titleb;
