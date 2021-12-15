eb758504d0ddb24ef0216954808506321fa29267
Kirihito <satowa.network@gmail.com>
2021-12-15 19:25:53 +0100

    Added header
    Der header besteht aus 2 Style sheet imports von mir und von prisma der besteht auch aus dem js import von prisma es wird wie bei allen anderen Projekten UTF-8 verwendet und es gibt ein php import namens other for php scripts die ganz oben sein müssen und überall verwendet werden

---
+++ assets/php/head.php
@@ -0,0 +1,11 @@
+<?php
+    require __DIR__.'/other.php';
+?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="http://127.0.0.1:1337/assets/css/style.css">
+<link rel="stylesheet" href="http://127.0.0.1:1337/assets/css/prism.css">
+<script src="http://127.0.0.1:1337/assets/js/script.js"></script>
+<!--<script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js"></script>-->
+<script src="http://127.0.0.1:1337/assets/js/prism.js"></script>
