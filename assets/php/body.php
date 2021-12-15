5dec34d906cf0129501740ae2eaa677d10c7ea31
Kirihito <satowa.network@gmail.com>
2021-12-15 19:23:45 +0100

    Added body
    Der body besteht aus einer Navbar die rechts von oben nach unten geht und nicht wie die meisten von links nach rechts mir war es wichtig das der so aussieht wie die meisten DOCS Navbars weil es einfach viel angenehmer ist zum auswählen und ja

---
+++ assets/php/body.php
@@ -0,0 +1,12 @@
+<div class="sidenav">
+    <h1>Satowa Network</h1>
+    <a href="https://docs/satowa-network.at/introduction">Introduction</a>
+    <a href="https://docs/satowa-network.at/introduction">Examples</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+</div>
+
+<!--<div class="container">
+    <h2>Sidenav Example</h2>
+    <p>This sidenav is always shown.</p>
+</div>-->
