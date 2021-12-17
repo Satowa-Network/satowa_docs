eebb1f9833117650e83e53cd213404043ee21e45
Kirihito <satowa.network@gmail.com>
2021-12-17 20:00:22 +0100

    Links wurden von Local auf public geändert

--- assets/php/head.php
+++ assets/php/head.php
@@ -4,8 +4,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="http://127.0.0.1:1337/assets/css/style.css">
-<link rel="stylesheet" href="http://127.0.0.1:1337/assets/css/prism.css">
-<script src="http://127.0.0.1:1337/assets/js/script.js"></script>
+<link rel="stylesheet" href="https://docs.satowa-network.at/assets/css/style.css">
+<link rel="stylesheet" href="https://docs.satowa-network.at/assets/css/prism.css">
+<script src="https://docs.satowa-network.at/assets/js/script.js"></script>
 <!--<script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js"></script>-->
-<script src="http://127.0.0.1:1337/assets/js/prism.js"></script>
+<script src="https://docs.satowa-network.at/assets/js/prism.js"></script>
