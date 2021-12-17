5189f661d1282dc8501abf1ee734040a77b11df1
Kirihito <satowa.network@gmail.com>
2021-12-18 00:30:36 +0100

    Update head.php
    Die header elemente wurden hinzugefügt

--- assets/php/head.php
+++ assets/php/head.php
@@ -2,8 +2,41 @@
     require __DIR__.'/other.php';
 ?>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<!--Font Awesome Import-->
+<script src="https://kit.fontawesome.com/537f8532c6.js" crossorigin="anonymous"></script>
+<!--Cookie Banner Import (PLS Don\'t remove it it\'s your Privacy ;-;)-->
+<script type="application/javascript" src="https://app.usercentrics.eu/latest/main.js" id="6vktZcgq7"></script>
+<!-- Website Title  -->
+<meta property="title" content="<?php $title ?>">
+<meta property="og:title" content="<?php $title ?>">
+<!-- Website Title on Twitter -->
+<meta name="twitter:title" content="<?php $title ?>">
+<!--Manifest File Import-->
+<link rel="manifest" href="https://satowa-network.at/assets/manifest/manifest.json">
+<!--Normal Description-->
+<meta name="description" content="Das Satowa Network ist ein Österreichisches Netzwerk für die Sicherheit im Internet. Das Satowa Network wurde 2022 gegründet mit dem Ziel Das Internet Sicherer zu machen und anderen Leuten zu Helfen.">
+<!-- Website Description -->
+<meta property="og:description" content="Das Satowa Network ist ein Österreichisches Netzwerk für die Sicherheit im Internet. Das Satowa Network wurde 2022 gegründet mit dem Ziel Das Internet Sicherer zu machen und anderen Leuten zu Helfen.">
+<!-- Website Image -->
+<meta property="og:image" content="https://satowa-network.at/assets/img/1.png">
+<!-- Set Website Viewport -->
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=5, minimum-scale=1.0">
+<!-- Theme Color for some Browser and Discord Embeds -->
+<meta name="theme-color" content="#6f0c8a">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- Disallow all Search Engines to Archive this site -->
+<!-- Set Website Icon -->
+<link rel="shortcut icon" href="https://satowa-network.at/assets/img/1.png" type="image/x-icon">
+<!-- third-generation iPad with high-resolution Retina display: -->
+<link rel="apple-touch-icon" sizes="144x144" href="https://satowa-network.at/assets/img/1.png">
+<!-- iPhone with high-resolution Retina display: -->
+<link rel="apple-touch-icon" sizes="114x114" href="https://satowa-network.at/assets/img/1.png">
+<!-- first- and second-generation iPad: -->
+<link rel="apple-touch-icon" sizes="72x72" href="https://satowa-network.at/assets/img/1.png">
+<!-- non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+<link rel="apple-touch-icon" href="https://satowa-network.at/assets/img/1.png">
+<!-- basic favicon -->
+<link rel="shortcut icon" href="https://satowa-network.at/assets/img/1.png">
 <link rel="stylesheet" href="https://docs.satowa-network.at/assets/css/style.css">
 <link rel="stylesheet" href="https://docs.satowa-network.at/assets/css/prism.css">
 <script src="https://docs.satowa-network.at/assets/js/script.js"></script>
