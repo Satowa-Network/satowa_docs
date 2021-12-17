ba1c9a20fe1bb6558ac4c7ffba240ddc90bcdab2
Kirihito <satowa.network@gmail.com>
2021-12-17 20:01:21 +0100

    Fehler fix
    Wird jetzt richtig weitergeleitet

--- assets/php/body.php
+++ assets/php/body.php
@@ -1,7 +1,7 @@
 <div class="sidenav">
     <h1>Satowa Network</h1>
-    <a href="https://docs/satowa-network.at/introduction">Introduction</a>
-    <a href="https://docs/satowa-network.at/introduction">Examples</a>
+    <a href="https://docs.satowa-network.at/index">Introduction</a>
+    <a href="https://docs.satowa-network.at/example">Examples</a>
     <a href="#">Clients</a>
     <a href="#">Contact</a>
 </div>
